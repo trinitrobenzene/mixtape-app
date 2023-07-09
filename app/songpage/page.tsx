0a02ca1affbeea98b26ea98daf2a0e46aed11048
Huu Thang <thangnh12d1@gmail.com>
2023-07-09 11:27:55 +0700

    songpage done

--- app/songpage/page.tsx
+++ app/songpage/page.tsx
@@ -10,29 +10,29 @@ import Comments from '@/components/Comments/Comments';
 
 const SongPage = () => {
 	return (
-		<div className="grid grid-rows-1">
+		<div className="flex flex-col">
 			<div className="absolute flex animate-slideup">
 				<AudioPlayer></AudioPlayer>
 			</div>
 			{/* <div className="h-100 bottom-0 left-0 right-0">
-        <div className="grid grid-cols-2">
-          <div className="flex flex-col bg-white m-3 py-6 rounded-lg space-y-3 relative h-fit">
-            <div className="flex justify-center p-8">
-              <Image src={img} alt="track cover" width={100} height={100} />
-            </div>
-            <span className="flex justify-center"> Nguyen Huu Thang</span>
-            <div className="flex justify-around grid-cols-2">
-              <div>
-                <People width={150} />
-              </div>
-              <Cassette width={150} />
-            </div>
-          </div>
-          <div>
-            <Comments></Comments>
-          </div>
-        </div>
-      </div> */}
+				<div className="grid grid-cols-2">
+					<div className="flex flex-col bg-white m-3 py-6 rounded-lg space-y-3 relative h-fit">
+						<div className="flex justify-center p-8">
+							<Image src={img} alt="track cover" width={100} height={100} />
+						</div>
+						<span className="flex justify-center"> Nguyen Huu Thang</span>
+						<div className="flex justify-around grid-cols-2">
+							<div>
+								<People width={150} />
+							</div>
+							<Cassette width={150} />
+						</div>
+					</div>
+					<div>
+						<Comments></Comments>
+					</div>
+				</div>
+			</div> */}
 		</div>
 	);
 };
